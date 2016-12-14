6859f05cf9d8bd33ef1705b9ed241b69f7745080
MaseKor <leynuxms@gmail.com>
2016-12-14 22:47:01 +0900

    Update README.md

--- test.py
+++
@@ -1,5 +0,0 @@
-from Minsung.Face import *
-
-data = ['https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/15420765_1828599787382899_1911295289473466631_n.jpg?oh=0a6e1655ebad885a3ca3cd25c5d23961&oe=58BB1C4F'
-        ,'https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/15178056_1804242483152153_3023187264783600884_n.jpg?oh=cded2e10e42df95a90a5601a19c32a9a&oe=58B51F87']
-detect(data)
