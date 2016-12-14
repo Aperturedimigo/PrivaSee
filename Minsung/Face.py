d48be89883842c85a64e5913633efc48cec623af
MaseKor <leynuxms@gmail.com>
2016-12-14 20:55:22 +0900

    Update all requests and add test.py

--- Minsung/Face.py
+++ Minsung/Face.py
@@ -1,6 +1,61 @@
 from Minsung.util import *
 
-def verify(face_id, another_face_id=None, person_group_id=None,person_id=None):
+def detect(image, face_id=True, landmarks=False, attributes=''):
+
+    url = 'detect'
+    headers, data, json = Image(image)
+    params = {
+        'returnFaceId': face_id and 'true' or 'false',
+        'returnFaceLandmarks': landmarks and 'true' or 'false',
+        'returnFaceAttributes': attributes,
+    }
+
+    return request('POST', url, headers=headers, params=params, json=json,
+                        data=data)
+
+
+def find_similars(face_id, face_list_id=None, face_ids=None,
+                  max_candidates_return=20, mode='matchPerson'):
+
+    url = 'findsimilars'
+    json = {
+        'faceId': face_id,
+        'faceListId': face_list_id,
+        'faceIds': face_ids,
+        'maxNumOfCandidatesReturned': max_candidates_return,
+        'mode': mode,
+    }
+
+    return request('POST', url, json=json)
+
+
+def group(face_ids):
+
+    url = 'group'
+    json = {
+        'faceIds': face_ids,
+    }
+
+    return request('POST', url, json=json)
+
+
+def identify(face_ids, person_group_id, max_candidates_return=1,
+             threshold=None):
+
+    url = 'identify'
+    json = {
+        'personGroupId': person_group_id,
+        'faceIds': face_ids,
+        'maxNumOfCandidatesReturned': max_candidates_return,
+        'confidenceThreshold': threshold,
+    }
+
+    return request('POST', url, json=json)
+
+
+def verify(face_id, another_face_id=None, person_group_id=None,
+           person_id=None):
+
     url = 'verify'
     json = {}
     if another_face_id:
